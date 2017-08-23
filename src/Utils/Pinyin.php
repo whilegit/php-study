8c5d3d664f50785d84f249c89802513e911f8550
Linzhongren <6215714@qq.com>
2017-08-23 16:10:47 +0800

    Pinyin:get()函数，实现gbk编码范围内的汉字转拼音（支持全拼和首拼）

---
+++ src/Utils/Pinyin.php
@@ -0,0 +1,95 @@
+<?php
+namespace Utils;
+/**
+ * 拼音类
+ * @author Administrator
+ * @desc <pre>
+ * 		gb2312双字节编码方案： 起始A1A2~F7FE，低字节总是小不于A0，高字节总不小于A1。从B0A1~F7FE为汉字编码，拼音相同的汉字总是分在同一分区，因些只要构造拼音分区的边界表，即能汉字转拼音了。
+ *      gbk编码兼容gb2312，起始0x8140~0xFEFF,低字节总是不小于0x40，高字节不小于0x81
+ *      
+ *      示例： Pinyin::get('中华人民共和国', true); 
+ * </pre>
+ */
+class Pinyin{
+	
+	//gb2312编码区的一级汉字区(第16~55区，共3755个汉字)，按拼音排序。gb2312的低数总是比0xA0大，高位范围为[0xB0,0xD7]
+	protected static $gb2312_class1_boundary = null;
+	//gb2312编码区的二级汉字节(第56~87区，共3008个汉字)，按部首/笔画排序
+	protected static $gb2312_class2_map = null;
+	//非gb2312的gbk汉字
+	protected static $gbk_map_without_gb2312 = null;
+	
+	/**
+	 * 汉字转拼音
+	 * @param string $str      待转换的汉字
+	 * @param string $whole    是否全拼
+	 * @param string $charset  待转换汉字的编码方式
+	 * @return mixed
+	 */
+	public static function get($str, $whole = true, $charset = 'utf-8'){
+		$charset = strtoupper($charset);
+		//如果是utf-8，则转成gb2312编码
+		if ($charset != 'GBK') {
+			$str = mb_convert_encoding($str, 'GBK', $charset);
+		}
+		$result = '';
+		$len = strlen($str);
+		$i = 0;
+		while ($i < $len){
+			$code = ord($str{$i++});
+			if ($code >= 0x81 || $code == 0x00){             //扩展到0x81以上是为了扩展到gbk，0x00表示ASCII码
+				$q = ord($str{$i++});
+				$code = ($code * 256) + $q;
+			}
+			$m = self::match($code);
+			if(empty($m)) continue;
+			$result .= $whole ? $m : $m{0};
+		}
+		return preg_replace('/[^a-z0-9]/', '', strtolower($result));
+	}
+	
+	
+	/**
+	 * 编码匹配拼音
+	 * @param int $code  范围[B0A1,F7FE]
+	 * @return string
+	 */
+	protected static function match($code){
+		$ret = '';
+		if ((0 < $code) && ($code < 80)){                   //普通ascii码
+			$ret = chr($code);
+		} else {
+			$h = $code >> 8;
+			$l = $code & 0x00FF;
+			if($h >= 0xB0 && $h <= 0xD7 && $l >= 0xA0 ){          //gb2312的一区汉字
+				if(self::$gb2312_class1_boundary == null){
+					self::$gb2312_class1_boundary = require(__DIR__ . '/../../static/pinyin/gb2312_class1_boundary.dat');
+				}
+				//此算法比常见算法性能要好
+				for($i = $code; $i>=45217; $i--){
+					if(isset(self::$gb2312_class1_boundary[$i])){
+						$ret = self::$gb2312_class1_boundary[$i];
+						break;
+					}
+				}
+			} else if($h >= 0xD8 && $h <= 0xF7 && $l >= 0xA0){  //gb2312的二区汉字
+				if(self::$gb2312_class2_map == null){
+					self::$gb2312_class2_map = require(__DIR__ . '/../../static/pinyin/gb2312_class2_map.dat');
+				}
+				if(isset(self::$gb2312_class2_map[$code])) {
+					$ret = $ret = self::$gb2312_class2_map[$code];
+				}
+			} else if($h >= 0x81) { 							//非gb2312的gbk汉字
+				if(self::$gbk_map_without_gb2312 == null){
+					self::$gbk_map_without_gb2312 = require(__DIR__ . '/../../static/pinyin/gbk_map_without_gb2312.dat');
+				}
+				if(isset(self::$gbk_map_without_gb2312[$code])){
+					$ret = self::$gbk_map_without_gb2312[$code];
+				}
+			} else {											
+				//无法识别
+			}
+		}
+		return $ret;
+	}
+}
--- test.php
+++ test.php
@@ -4,9 +4,27 @@ use Utils\Comm;
 use Utils\Trace;
 use Utils\IString;
 use Utils\Sms;
+use Utils\Excel;
+use Utils\Pinyin;
+
 require_once "vendor/autoload.php";
 
+
+$list = array(
+		array('a'=>1, 'b'=>2),
+		array('a'=>3, 'b'=>4),
+);
+$params = array(
+		'columns'=>array(
+				array('field'=>'a', 'width'=>'24', 'title'=>'项目1'),
+				array('field'=>'b', 'width'=>'24', 'title'=>'项目2'),
+		),
+		'title' => '项目表'
+);
+//Excel::export($list, $params);
+
 Misc::cookie(Misc::random(6), 'test cookies, might be timeout in 60 seconds', 60);
+
 ?>
 <html>
 <head><meta charset="utf-8"><title>测试</title></head>
@@ -41,9 +59,9 @@ if(isset($ret['success']) && $ret['success'] == false){
 }else{
 	echo 'Http访问测试:  '.var_export($ret['responseline'], true)."<br />\r\n";
 }
+
 Trace::monolog('m.log');
 Trace::log($config);
-//Trace::out($config);
 
 $str = "string\'a\"\\\'";
 echo "IString::stripslashes_deep转义： 前[ {$str} ]  后 [ " . IString::stripslashes_deep($str) . " ]<br />\r\n";
@@ -53,76 +71,12 @@ echo "IString::ihtmlspecialchars转义： 前<input type='text' value='{$str}'/>
 echo "Misc::referer: " . Misc::referer() . '，$_SERVER[\'HTTP_REFERER\']='.  $_SERVER['HTTP_REFERER'] . "<br />\r\n";
 echo "Misc::token: " . Misc::token() . "<br />\r\n";
 
-//Sms::send();
 Sms::init('LTAIuK5a7gprq5rd','moMRNbrKlvSoqxI9uqQiLg94z0zJRr');
-//Trace::out(Sms::senda());
 //Trace::out(Sms::send('林忠仁', 'SMS_86610162', '18968596872', array('code'=>'123456')));
-
 //Trace::out(Sms::query('18968596872', date('Ymd'), 10, 1));
 
-/** Error reporting */
-error_reporting(E_ALL);
-ini_set('display_errors', TRUE);
-ini_set('display_startup_errors', TRUE);
-date_default_timezone_set('Europe/London');
-define('EOL',(PHP_SAPI == 'cli') ? PHP_EOL : '<br />');
-/** Include PHPExcel */
+echo "'要好岙屌貂'的全拼是：" . Pinyin::get("要好岙屌貂") . "<br />\r\n";
 
-// Create new PHPExcel object
-echo date('H:i:s') , " Create new PHPExcel object" , EOL;
-$objPHPExcel = new PHPExcel();
-// Set document properties
-echo date('H:i:s') , " Set document properties" , EOL;
-$objPHPExcel->getProperties()->setCreator("Maarten Balliauw")
-->setLastModifiedBy("Maarten Balliauw")
-->setTitle("PHPExcel Test Document")
-->setSubject("PHPExcel Test Document")
-->setDescription("Test document for PHPExcel, generated using PHP classes.")
-->setKeywords("office PHPExcel php")
-->setCategory("Test result file");
-// Add some data
-echo date('H:i:s') , " Add some data" , EOL;
-$objPHPExcel->setActiveSheetIndex(0)
-->setCellValue('A1', 'Hello')
-->setCellValue('B2', 'world!')
-->setCellValue('C1', 'Hello')
-->setCellValue('D2', 'world!');
-// Miscellaneous glyphs, UTF-8
-$objPHPExcel->setActiveSheetIndex(0)
-->setCellValue('A4', 'Miscellaneous glyphs')
-->setCellValue('A5', 'éàèùâêîôûëïüÿäöüç');
-// Rename worksheet
-echo date('H:i:s') , " Rename worksheet" , EOL;
-$objPHPExcel->getActiveSheet()->setTitle('Simple');
-// Set active sheet index to the first sheet, so Excel opens this as the first sheet
-$objPHPExcel->setActiveSheetIndex(0);
-// Save Excel 2007 file
-echo date('H:i:s') , " Write to Excel2007 format" , EOL;
-$callStartTime = microtime(true);
-$objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');
-$objWriter->save(str_replace('.php', '.xlsx', __FILE__));
-$callEndTime = microtime(true);
-$callTime = $callEndTime - $callStartTime;
-echo date('H:i:s') , " File written to " , str_replace('.php', '.xlsx', pathinfo(__FILE__, PATHINFO_BASENAME)) , EOL;
-echo 'Call time to write Workbook was ' , sprintf('%.4f',$callTime) , " seconds" , EOL;
-// Echo memory usage
-echo date('H:i:s') , ' Current memory usage: ' , (memory_get_usage(true) / 1024 / 1024) , " MB" , EOL;
-// Save Excel5 file
-echo date('H:i:s') , " Write to Excel5 format" , EOL;
-$callStartTime = microtime(true);
-$objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel5');
-$objWriter->save(str_replace('.php', '.xls', __FILE__));
-$callEndTime = microtime(true);
-$callTime = $callEndTime - $callStartTime;
-echo date('H:i:s') , " File written to " , str_replace('.php', '.xls', pathinfo(__FILE__, PATHINFO_BASENAME)) , EOL;
-echo 'Call time to write Workbook was ' , sprintf('%.4f',$callTime) , " seconds" , EOL;
-// Echo memory usage
-echo date('H:i:s') , ' Current memory usage: ' , (memory_get_usage(true) / 1024 / 1024) , " MB" , EOL;
-// Echo memory peak usage
-echo date('H:i:s') , " Peak memory usage: " , (memory_get_peak_usage(true) / 1024 / 1024) , " MB" , EOL;
-// Echo done
-echo date('H:i:s') , " Done writing files" , EOL;
-echo 'Files have been created in ' , getcwd() , EOL;
 
 
 ?>
